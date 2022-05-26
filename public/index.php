55e11cfaf87db913fe7f65732c4766e48b41ecdb
JoaoPedroCorrea <joaocorrea666@gmail.com>
2022-05-26 12:15:35 -0300

    Model functions

--- public/index.php
+++ public/index.php
@@ -1,5 +1,10 @@
 <?php
 
-require_once(dirname(__FILE__, 2) . '/src/config/database.php');
+require_once(dirname(__FILE__, 2) . '/src/config/config.php');
+require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
-Database::getConnection();
+$user = new User(['name' => 'João', 'email' => 'joaocorrea666@gmail.com']);
+
+// echo $user->getSelect();
+
+echo User::getSelect(['name' => 'Chaves', 'email' => 'chaves@cod3r.com.br']);
