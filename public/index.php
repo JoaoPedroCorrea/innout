4d370926eaa186610142149f270dd9586db12e47
JoaoPedroCorrea <joaocorrea666@gmail.com>
2022-05-27 15:54:38 -0300

    templates

--- public/index.php
+++ public/index.php
@@ -1,12 +1,10 @@
 <?php
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 
-// echo $_SERVER['REQUEST_URI'];
+$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
-$uri = urldecode($_SERVER['REQUEST_URI']);
-
-if($uri === '/' || $uri === ''){
+if($uri === '/' || $uri === '' || $uri === '/index.php'){
     $uri = '/loginController.php';
 }
-// echo CONTROLLER_PATH . $uri;
+
 require_once(CONTROLLER_PATH . "{$uri}");
