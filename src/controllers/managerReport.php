8799d5f46fc2770468c287317742b80a26aa577e
JoaoPedroCorrea <joaocorrea666@gmail.com>
2022-06-06 14:54:30 -0300

    Manager Reports finished

--- src/controllers/managerReport.php
+++ src/controllers/managerReport.php
@@ -2,4 +2,15 @@
 session_start();
 requireValidSession();
 
-loadTemplateView('managerReport', []);
+$activeUsersCount = User::getActiveUsersCount();
+$absentUsers = WorkingHours::getAbsentUsers();
+
+$yearAndMonth = (new DateTime())->format('Y-m');
+$seconds = WorkingHours::getWorkedTimeInMonth($yearAndMonth);
+$hoursInMonth = explode(':', getTimeStringFromSeconds($seconds))[0];
+
+loadTemplateView('managerReport', [
+    'activeUsersCount' => $activeUsersCount,
+    'absentUsers' => $absentUsers,
+    'hoursInMonth' => $hoursInMonth,
+]);
